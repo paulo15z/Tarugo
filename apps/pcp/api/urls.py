03937237c51ba9ded3c7e82f3d365cdff4eb8c37
paulo15z <paulociee123@gmail.com>
2026-03-24 22:20:52 -0300

    1

--- apps/pcp/api/urls.py
+++ apps/pcp/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from apps.pcp.api.views import PCPProcessView, PCPDownloadView, PCPHistoricoView
 from django.views.generic import TemplateView
+
 urlpatterns = [
     path('processar/', PCPProcessView.as_view(), name='pcp-processar'),
     path('download/<str:pid>/', PCPDownloadView.as_view(), name='pcp-download'),
