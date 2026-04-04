298c68b0d0959d09ada5f776ba42370d93042153
paulo15z <173558143+paulo15z@users.noreply.github.com>
2026-04-03 22:11:09 -0400

    fix: register pcp namespace and fix NoReverseMatch errors

--- apps/pcp/urls.py
+++ apps/pcp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'pcp'
+
 urlpatterns = [
-    path('',            views.pcp_index,     name='pcp-index'),
-    path('processar/',  views.pcp_processar, name='pcp-processar'),
-    path('historico/',  views.pcp_historico, name='pcp-historico'),
-    path('liberar/<str:pid>/', views.pcp_liberar, name='pcp-liberar'),
-    path('liberar-viagem/<str:pid>/', views.pcp_liberar_viagem, name='pcp-liberar-viagem'),
-    path('download/<str:pid>/', views.pcp_download, name='pcp-download'),
+    path('',            views.pcp_index,     name='index'),
+    path('processar/',  views.pcp_processar, name='processar'),
+    path('historico/',  views.pcp_historico, name='historico'),
+    path('liberar/<str:pid>/', views.pcp_liberar, name='liberar'),
+    path('liberar-viagem/<str:pid>/', views.pcp_liberar_viagem, name='liberar-viagem'),
+    path('download/<str:pid>/', views.pcp_download, name='download'),
 ]
